1b8bf1c298b11871c14f366439095a6c0f8f67a1
Mohamed Samir <mohamed4samir994@gmail.com>
2022-08-03 02:55:12 +0200

    Scrap B&N for books & post it to db

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -2,12 +2,10 @@ const axios     = require('axios'),
     cheerio     = require('cheerio'),
     DomParser   = require('dom-parser'),
     parser      = new DomParser(),
-    handleJSON  = require('./handle-json-file');
-
-
-const pages = [
-    'https://www.barnesandnoble.com/w/where-the-crawdads-sing-delia-owens/1127681226?ean=9780735219106',
-];
+    handleJSON  = require('./handle-json-file'),
+    homePageUrl =  "https://www.barnesandnoble.com",
+     pages      = require("./booksUrl.json"),
+     booksJson= require("./books.json");
 
 let currentDom;
 
@@ -17,12 +15,34 @@ const loopThroughPages = async (pages) => {
     pages.map( async (page) => {
         try {
             const book = await getPage(page);
-            handleJSON.add("books.json", book);
+            if(book){
+                handleJSON.add("books.json", book);
+            }
         } catch (err) {
             console.error(err.message);
         }
     });
 }
+
+const getAllBooks=async ()=>{
+    try {
+    handleJSON.createFile("booksUrl.json");
+    const homePage=await axios.get(homePageUrl);
+   currentDom     = parseHTML(homePage.data),
+    tagNames       = currentDom.getElementsByTagName("a");
+    tagNames.map((tagName)=>{
+    if(tagName.getAttribute("title")=== "view details"){
+        const bookUrl= homePageUrl+ tagName.getAttribute("href");
+            handleJSON.add("booksUrl.json",bookUrl)
+        };
+    })
+}catch (err){
+console.log(err);
+}
+}
+
+
+
 
 const getPage = async (pageUrl) => {
     try {
@@ -67,4 +87,15 @@ const generateRating = () => {
     return Math.floor(Math.random() * 5) + 1;
 }
 
-loopThroughPages(pages);
+const postToApi=(books)=>{
+books.map(async(book)=>{
+try{
+const req=await axios.post("http://127.0.0.1:5003/products",book)
+return console.log(req.data);
+}catch(err){
+console.log(err.data);
+}
+})
+}
+postToApi(booksJson)
+// loopThroughPages(pages);
