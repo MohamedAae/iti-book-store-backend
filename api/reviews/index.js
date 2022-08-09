4739c27f04be193cfbe5be4f1fef2e916946d40c
Youssef-AbuTaleb <youssefabdelmoneim134@gmail.com>
2022-08-09 21:53:20 +0200

    Add getReviewsbyId to reviews endpoint

--- api/reviews/index.js
+++ api/reviews/index.js
@@ -1,10 +1,11 @@
 const express = require("express"),
   router = express.Router(),
   controller = require("./reviews.controller"),
-    jwt = require("../../auth/token");
+  jwt = require("../../auth/token");
 
 router.get("/", controller.get);
 router.post("/", jwt.auth, controller.create);
 router.get("/:id", controller.getById);
+router.get("/user/:id", controller.getReviewsByUserId);
 
-module.exports=router
+module.exports = router;
--- api/reviews/reviews.controller.js
+++ api/reviews/reviews.controller.js
@@ -1,52 +1,66 @@
-const   helpers = require("../../helpers/api.js"),
-        mongoose = require("mongoose"),
-        Reviews = require("../reviews/reviews.model");
+const helpers = require("../../helpers/api.js"),
+  mongoose = require("mongoose"),
+  Reviews = require("../reviews/reviews.model");
 
-
-const Controller ={
-    get:async (req,res,next)=>{
-        try{
-            const reviews=await Reviews.find()
-            return res.status(201).json({
-                success: true,
-                code: 201,
-                reviews: reviews
+const Controller = {
+  get: async (req, res, next) => {
+    try {
+      const reviews = await Reviews.find();
+      return res.status(201).json({
+        success: true,
+        code: 201,
+        reviews: reviews,
       });
-        }catch(err){
-          return  helpers.handleError(err,res)
-        }
-    },
-
-    getById:async (req,res,next)=>{
-        const id = req.params.id;
-        try {
-            const reviews = await Reviews.find({bookId: id}).populate("userId");
-            return res.status(201).json({
-                success: true,
-                code: 201,
-                reviews: reviews
-            });
-        }catch(err){
-            return helpers.handleError(err,res)
-        }
-    },
-
-    create:async (req,res,next)=>{
-        const data      = req.body;
-        data.date       = new Date()
-
-        const review    = new Reviews(data);
-        try {
-            const saved = await review.save()
-            return res.status(201).json({
-                success : true,
-                code    : 201,
-                review  : saved
-            });
-        } catch(err) {
-            return helpers.handleError(err,res);
-        }
+    } catch (err) {
+      return helpers.handleError(err, res);
     }
-}
+  },
 
-module.exports=Controller
+  getById: async (req, res, next) => {
+    const id = req.params.id;
+    try {
+      const reviews = await Reviews.find({ bookId: id }).populate("userId");
+      return res.status(201).json({
+        success: true,
+        code: 201,
+        reviews: reviews,
+      });
+    } catch (err) {
+      return helpers.handleError(err, res);
+    }
+  },
+
+  getReviewsByUserId: async (req, res, next) => {
+    const id = req.params.id;
+
+    try {
+      const reviews = await Reviews.find({ userId: id }).populate("bookId");
+      return res.status(201).json({
+        success: true,
+        code: 201,
+        reviews: reviews,
+      });
+    } catch (err) {
+      return helpers.handleError(err, res);
+    }
+  },
+
+  create: async (req, res, next) => {
+    const data = req.body;
+    data.date = new Date();
+
+    const review = new Reviews(data);
+    try {
+      const saved = await review.save();
+      return res.status(201).json({
+        success: true,
+        code: 201,
+        review: saved,
+      });
+    } catch (err) {
+      return helpers.handleError(err, res);
+    }
+  },
+};
+
+module.exports = Controller;
